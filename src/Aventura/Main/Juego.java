655e0e5d425747fc8b60dc296fc9bd4c3ad81f51
Manu <109155188+Manolilloo@users.noreply.github.com>
2026-01-20 10:39:02 +0100

    descripcion del juego

--- src/Aventura/Main/Juego.java
+++ src/Aventura/Main/Juego.java
@@ -13,15 +13,16 @@ public class Juego {
     // --- NÚCLEO: Definición de Datos (FASE 1) ---
     // Esta parte os la damos HECHA. Es el "contrato" del núcleo.
 
-    private static String descripcionJuego = "No sabes qué ha pasado. Justo cuando terminabas las clases te quedaste el último como siempre recogiendo tus cosas. " +
-            "Pero algo pasó. Lo último que recuerdas es que sentiste mucho frío y todo se volvió oscuro. Ahora estás en tu clase, pero es de noche y el instituto está cerrado." +
-            "¿Nadie te ha visto? ¿Por qué las limpiadoras no te han despertado?";
+    private static String descripcionJuego = "Estabas cagando en el baño de un Mercadona random, tenías mucho sueño porque habías estado\\n toda la noche jugando al call of duty," +
+            " de repente notas cómo tus parpados empiezan a cerrarse. Al final te quedas dormido por viciar toda\\n la noche y resulta que, cuando te despiertas, en vez de" +
+            " estar sentado en aquel váter estas en una especie de sillón\\n en una sala totalmente a oscuras. Cuando te levantas, se te enciende la sala en la que estás. Ya no estás " +
+            "en aquel baño cutre del Mercadona,\\n ahora estabas en un lugar totalmente desconocido.";
 
     // El mapa de habitaciones.
     // TODO: (Skin) ¡Rellenad esto con vuestras descripciones!
     private static String[] habitaciones = {
-            "Estás en tu aula, el aula 103. Hay una puerta a la DERECHA.",  // Posición 0
-            "Estás en el pasillo principal. Hay puertas a la DERECHA y a la IZQUIDA.", // Posición 1
+            "Estás en una sala . Hay una puerta a la DERECHA.",  // Posición 0
+            "Estás en el pasillo principal. Hay puertas a la DERECHA y a la IZQUIERDA.", // Posición 1
             "Estás en el aula 105. Hay una puerta a la IZQUIERDA y has visto una 'llave' en una mesa.", // Posición 2
             // Borra las habitaciones y escribe las tuyas
     };
