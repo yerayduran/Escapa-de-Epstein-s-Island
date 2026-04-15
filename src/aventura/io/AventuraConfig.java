b03c3336549d7d6f236c013f4c7287daece1ca37
Manu <109155188+Manolilloo@users.noreply.github.com>
2026-04-15 09:41:29 +0200

    Creacion de la clase AventuraConfig pero me queda el setter

---
+++ src/aventura/io/AventuraConfig.java
@@ -0,0 +1,44 @@
+/**
+ * @author ManuelPerez
+ * @version 1.0
+ */
+
+package aventura.io;
+
+import aventura.domain.Habitacion;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class AventuraConfig {
+
+    private String descripcionGeneral;
+    private String habitacionInicial;
+    private Map<String, Habitacion> habitaciones;
+
+    public AventuraConfig() {
+        this.habitaciones = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+    }
+
+    public AventuraConfig(String descripcionGeneral, String habitacionInicial, Map<String, Habitacion> habitaciones) {
+        this.descripcionGeneral = descripcionGeneral;
+        this.habitacionInicial = habitacionInicial;
+        this.habitaciones = habitaciones;
+    }
+
+    public String getDescripcionGeneral() {
+        return descripcionGeneral;
+    }
+
+
+    public String getHabitacionInicial() {
+        return habitacionInicial;
+
+    }
+
+    public Map<String, Habitacion> getHabitaciones() {
+        return habitaciones;
+    }
+
+
+}
