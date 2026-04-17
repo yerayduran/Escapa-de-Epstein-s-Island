61e21fe3151bc6a8a7f8d040dbf4a59598a1cfa2
yerayduran <yerayduranchaves2006@gmail.com>
2026-04-17 09:09:48 +0200

    Correcion de la clase de varias clases de domain y de io.java

--- src/main/java/aventura/io/ObjetoAdapter.java
+++ src/main/java/aventura/io/ObjetoAdapter.java
@@ -8,7 +8,7 @@ import aventura.domain.Llave;
 import aventura.domain.Mueble;
 import aventura.domain.Nota;
 import aventura.domain.Objeto;
-import aventura.domain.Pilas;
+import aventura.domain.Velas;
 import aventura.domain.Puerta;
 
 import com.google.gson.JsonDeserializationContext;
@@ -32,9 +32,7 @@ public class ObjetoAdapter implements JsonSerializer<Objeto>, JsonDeserializer<O
         if (src instanceof Artefacto) {
             json.addProperty(CAMPO_TIPO, "artefacto");
         } else if (src instanceof FragmentoRitual) {
-            json.addProperty(CAMPO_TIPO, "fragmentoRitual");
-        } else if (src instanceof Pilas) {
-            json.addProperty(CAMPO_TIPO, "pilas");
+            json.addProperty(CAMPO_TIPO, "fragmento");
         } else if (src instanceof Puerta) {
             json.addProperty(CAMPO_TIPO, "puerta");
         } else if (src instanceof Contenedor) {
@@ -43,6 +41,8 @@ public class ObjetoAdapter implements JsonSerializer<Objeto>, JsonDeserializer<O
             json.addProperty(CAMPO_TIPO, "nota");
         } else if (src instanceof Llave) {
             json.addProperty(CAMPO_TIPO, "llave");
+        } else if (src instanceof Velas) {
+            json.addProperty(CAMPO_TIPO, "velas");
         } else if (src instanceof Mueble) {
             json.addProperty(CAMPO_TIPO, "mueble");
         } else if (src instanceof Item) {
@@ -68,12 +68,12 @@ public class ObjetoAdapter implements JsonSerializer<Objeto>, JsonDeserializer<O
 
         return switch (tipo) {
             case "artefacto" -> contexto.deserialize(jsonObjeto, Artefacto.class);
-            case "fragmentoritual" -> contexto.deserialize(jsonObjeto, FragmentoRitual.class);
-            case "pilas" -> contexto.deserialize(jsonObjeto, Pilas.class);
+            case "fragmento" -> contexto.deserialize(jsonObjeto, FragmentoRitual.class);
             case "puerta" -> contexto.deserialize(jsonObjeto, Puerta.class);
             case "contenedor" -> contexto.deserialize(jsonObjeto, Contenedor.class);
             case "nota" -> contexto.deserialize(jsonObjeto, Nota.class);
             case "llave" -> contexto.deserialize(jsonObjeto, Llave.class);
+            case "velas" -> contexto.deserialize(jsonObjeto, Velas.class);
             case "mueble" -> contexto.deserialize(jsonObjeto, Mueble.class);
             case "item" -> contexto.deserialize(jsonObjeto, Item.class);
             default -> throw new JsonParseException("Tipo de objeto desconocido: " + tipo);
